9d686b29dbe88f740b3f3a07d078209aa94cfd6c
tejbari5 <136843098+tejbari5@users.noreply.github.com>
2023-11-01 10:28:13 +0530

    updated

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["FRONTEND DEVELOPER", "WEB DEVELOPER", "SOFTWARE DEVELOPER"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
