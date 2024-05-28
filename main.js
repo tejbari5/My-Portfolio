fff61fbea5f3b7fb448135c7dd51bbbf40c82964
tejbari5 <136843098+tejbari5@users.noreply.github.com>
2024-05-28 10:14:07 +0530

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings:["FRONTEND DEVELOPER", "WEB DEVELOPER", "SOFTWARE DEVELOPER"],
+    strings:["MERN STACK DEVELOPER", "WEB DEVELOPER", "SOFTWARE DEVELOPER"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
